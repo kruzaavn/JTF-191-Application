9b24922bd04128fd6714e516e5a6a3643107480d
akruza <akruza@groupw.com>
2019-09-04 15:30:58 -0400

    included veutify and vuex

--- vue/app-ui/src/main.js
+++ vue/app-ui/src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuex from  'vuex'
 import vuetify from './plugins/vuetify';
+import io from 'socket.io-client'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
@@ -11,3 +12,15 @@ new Vue({
 }).$mount('#app');
 
 Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count ++
+    }
+  }
+
+});
