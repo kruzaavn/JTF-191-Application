7d29a7327d030db6c6af63e734069bba9ac47b68
akruza <akruza@groupw.com>
2019-09-04 14:42:12 -0400

    included veutify and vuex

--- vue/app-ui/src/main.js
+++ vue/app-ui/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from  'vuex'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: h => h(App)
+}).$mount('#app');
+
+Vue.use(Vuex);
