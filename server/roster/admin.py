b95e366c7fab874a41b518a6f810bd15a9ac7a16
kruzaavn <aleks.kruza@gmail.com>
2020-02-09 16:00:45 -0500

    added model serializers

--- server/roster/admin.py
+++ server/roster/admin.py
@@ -2,3 +2,28 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+
+
+@admin.register(HQ)
+class HQAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AirFrame)
+class AirFrameAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Squadron)
+class SquadronAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Operation)
+class HQAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Aviator)
+class HQAdmin(admin.ModelAdmin):
+    pass
