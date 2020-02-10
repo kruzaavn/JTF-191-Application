affdbe3a29dbc039159142a3308d40631269a7e9
akruza <akruza@groupw.com>
2020-02-10 14:31:04 -0500

    Server update

--- server/roster/urls.py
+++ server/roster/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+from .views import *
 
 urlpatterns = [
 
-    # path('roster/', include('roster.urls'))
+    path('roster/aviators/', AviatorListView.as_view())
 ]
--- server/roster/views.py
+++ server/roster/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView
+from .models import Aviator
+from .serializers import AviatorSerializer
 # Create your views here.
+
+
+class AviatorListView(ListCreateAPIView):
+
+    queryset = Aviator.objects.all()
+    serializer_class = AviatorSerializer
