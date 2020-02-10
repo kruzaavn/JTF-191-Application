29d7267d569c1cc8d4e23d4f9dc3660b88d8e8f3
akruza <akruza@groupw.com>
2020-02-10 14:00:31 -0500

    Server update

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('roster/', include('roster.urls')),
     path('gci/', include('gci.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
+
