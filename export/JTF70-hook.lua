3a01bb5d8d55f7fd49536c8b8be40a4dbafff5ce
kruzaavn <aleks.kruza@gmail.com>
2020-06-11 17:11:07 -0400

    update lua hook script

--- export/JTF70-hook.lua
+++ export/JTF70-hook.lua
@@ -1,40 +1,10 @@
 
-
-
 local export_file
 local c
 local socket
 local JSON
 
 -- helper functions
-
-function LuaObject2Json(object)
-
-	local o = object
-
-	local message = '{'
-
-	for k,v in pairs(o) do
-		local json
-		local previous_loop_message
-		json = JSON:encode(v)
-		json = string.format('"%d": %s,', k, json)
-		previous_loop_message = message
-		message = previous_loop_message .. json
-
-	end
-	message = string.sub(message,1, -2) ..'}'
-
-	return message
-end
-
-function Export2File(message)
-
-	if export_file then
-		export_file:write(message .. '\n')
-	end
-
-end
 
 function Export2Socket(message)
 
@@ -55,7 +25,6 @@ function ExportWorldObjects(t)
 
 		message = string.format('{"%d":{"sim_time": %d, "states": %s}}',k, t, json)
 
-		Export2File(message)
 		Export2Socket(message)
 
 	end
@@ -69,36 +38,40 @@ function connect_socket()
 		c = socket.try(socket.connect(host, port)) -- connect to the listener socket
 		c:setoption("tcp-nodelay",true) -- set immediate transmission mode
 	end
+
 end
 
 -- export required functions
 
 function LuaExportStart()
--- Works once just before mission start.
 
--- 1) File options, if you dont want to export a file to the hard drive comment the flowing line out
+	-- Works once just before mission start.
 
-	-- export_file = io.open("C:/Users/Administrator/Saved Games/DCS.openbeta_server/Logs/Export.log", "w")
-
--- 2) Socket
+	-- Socket config
 	local host
 	local port
+	JSON = require('JSON')
 
 	package.path  = package.path..";"..lfs.currentdir().."/LuaSocket/?.lua" .. ';' ..lfs.currentdir().. '/Scripts/?.lua'
 	package.cpath = package.cpath..";"..lfs.currentdir().."/LuaSocket/?.dll"
 	socket = require("socket")
+
 	host = host or "localhost"
 	port = port or 8081
+
 	connect_socket()
-	JSON = require('JSON')
+
 end
 
 
 function LuaExportActivityNextEvent(t)
+
 	local tNext = t
 
 	if t % 30 == 0 then
+
 		connect_socket()
+
 	end
 
 	-- export functions
@@ -113,13 +86,8 @@ end
 
 function LuaExportStop()
 -- Works once just after mission stop.
--- Close files and/or connections here.
--- 1) File
-	if export_file then
-		export_file:close()
-		export_file = nil
-	end
--- 2) Socket
+
+-- Socket
 	if c then
 		c:close()
 		c = nil
