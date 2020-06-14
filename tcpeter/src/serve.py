84e4d392f73116911131e471003b9e5c969e7d41
kruzaavn <aleks.kruza@gmail.com>
2020-06-13 23:39:39 -0400

    added websocket to tcpeter

--- tcpeter/src/serve.py
+++ tcpeter/src/serve.py
@@ -1,24 +1,32 @@
 import os
 import uuid
+import datetime
+import json
 from tornado.tcpserver import TCPServer
 from tornado.websocket import websocket_connect
 from tornado.iostream import StreamClosedError
 from tornado.ioloop import IOLoop
-import datetime
+
 
 websockets = {}
+
+
+def log(string):
+    return f'{datetime.datetime.now}:{string}'
 
 
 class TCPeter(TCPServer):
 
     async def handle_stream(self, stream, address):
-
-        websockets[address[0]] = websocket_connect(f'ws://localhost:8000/ws/chat/BOB/')
+        log(f'connected to {address[0]}')
+        websockets[address[0]] = await websocket_connect(f'ws://172.18.0.1:8000/ws/chat/BOB/')
+        log(f'{websockets[address[0]]}')
 
         while True:
             try:
                 data = await stream.read_until(b"\n")
-                print(data, websockets[address[0]])
+                message = {"message": json.loads(data)}
+                websockets[address[0]].write_message(json.dumps(message))
 
             except StreamClosedError:
                 break
@@ -28,7 +36,7 @@ if __name__ == '__main__':
 
     server = TCPeter()
     port = int(os.getenv('PORT', 7224))
-    print(f'{datetime.datetime.now()} listening on {port}')
+    log(f'listening on {port}')
     server.bind(port)
     server.start()
     IOLoop.current().start()
