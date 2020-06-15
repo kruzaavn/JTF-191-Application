1d81753c5444095e4f09350e4b6afbea8f50d942
akruza <akruza@groupw.com>
2020-06-14 23:43:29 -0400

    restarted ui

---
+++ ui/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+
+  </v-app>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
