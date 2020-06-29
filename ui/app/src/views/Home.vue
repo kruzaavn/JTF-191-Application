a13757302d9b2f97e67a56d0e051b1a03b8cadb0
kruzaavn <aleks.kruza@gmail.com>
2020-06-29 12:33:44 -0400

    update api-server my page resources

--- ui/app/src/views/Home.vue
+++ ui/app/src/views/Home.vue
@@ -1,26 +1,24 @@
 <template>
-    <v-container >
-        <v-row align-content="start">
-            <v-col>
-                <h1>Operational Squadrons</h1>
-            </v-col>
-        </v-row>
-            <v-row
-
-
-            >
-                <v-col
-                        v-for="squadron in squadrons"
+    <v-container>
+        <v-row align-content="start" v-for="(squadron_type, index) in squadron_types" :key="index">
+                <v-container>
+                    <v-row>
+                    <h1>{{squadron_type[0].toUpperCase() + squadron_type.slice(1) }} Squadrons</h1>
+                        </v-row>
+                    <v-row>
+                    <v-col cols="3" v-for="squadron in squadrons.filter(x => x.type === squadron_type)"
                         :key="squadron.id"
-                        cols="3"
-                >
+
+                    >
                     <v-card
-                        max-width="450"
+
+                        width="450"
                         class="my-4 mx-4 py-2 px-2"
                         :to="'/squadron/' + squadron.designation"
                         dark
                         color="#00000077"
                         tile
+
                     >
                         <v-img
                                 contain
@@ -39,15 +37,14 @@
                             {{squadron.designation}}
                         </v-card-subtitle>
                     </v-card>
-                </v-col>
+                        </v-col>
+                        </v-row>
+                </v-container>
             </v-row>
-
     </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-
 import { mapGetters, mapActions } from 'vuex'
 
 export default {
@@ -63,6 +60,12 @@ export default {
   },
   computed: {
       ...mapGetters(['squadrons']),
+      squadron_types: function () {
+          // this function returns a unique set of squadron types returned by the api at /api/roster/squadrons/list/
+
+          let squadron_types = this.squadrons.map(squadron => squadron.type)
+          return [ ...new Set(squadron_types)]
+      }
 
   }
 }
