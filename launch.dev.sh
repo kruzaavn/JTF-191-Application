1d73608796551730834578792ec6455f4522bb81
Ryan Breaker <ryan@breaker.rocks>
2020-07-02 22:41:34 -0500

    feat: complete https support via nginx proxy

--- launch.dev.sh
+++ launch.dev.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 
-docker-compose -f docker-compose.yaml build
-docker-compose -f docker-compose.yaml up -d
+docker-compose -f docker-compose.yaml up -d --build
--- launch.prod.sh
+++ launch.prod.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 
-docker-compose -f docker-compose.yaml -f docker-compose.prod.yaml build
-docker-compose -f docker-compose.yaml -f docker-compose.prod.yaml up -d
+docker-compose -f docker-compose.yaml -f docker-compose.prod.yaml up -d --build
