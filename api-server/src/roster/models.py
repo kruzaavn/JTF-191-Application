05549b2aee959574b24f613c0b8f371242168def
kruzaavn <aleks.kruza@gmail.com>
2020-08-12 12:54:09 -0400

    updated kills and hours field and updated to general JSON field

--- api-server/src/roster/models.py
+++ api-server/src/roster/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
 from datetime import datetime
@@ -140,7 +141,7 @@ class Aviator(Pilot):
     tail_number = models.CharField(max_length=64, blank=True, null=True)
     position_code = models.IntegerField(default=4, validators=[MinValueValidator(1), MaxValueValidator(4)])
     user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
-    stats = models.JSONField(default=stats_default)
+    stats = JSONField(default=stats_default)
 
     @property
     def rank(self):
