9b163ca423fcdbed3032cf4473b4e97c7fb832de
kruzaavn <aleks.kruza@gmail.com>
2020-11-24 12:16:53 -0500

    production config and secrets implemented

--- api-server/src/server/settings.py
+++ api-server/src/server/settings.py
@@ -13,20 +13,33 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import pathlib
 import json
+import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+SECRET_FILE = pathlib.Path(BASE_DIR).joinpath('secrets.txt')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
+PRODUCTION = os.getenv('PRODUCTION')
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%yg))7dqy#6c=l2cu+#%28tfg4p1n29c7xwc5%%tk8*hbv2o2e'
+
+if PRODUCTION is None:
+    SECRET_KEY = '%yg))7dqy#6c=l2cu+#%28tfg4p1n29c7xwc5%%tk8*hbv2o2e'
+
+elif PRODUCTION is not None and SECRET_FILE.is_file():
+    SECRET_KEY = SECRET_FILE.read_text()
+
+else:
+    SECRET_KEY = secrets.token_urlsafe(64)
+    SECRET_FILE.write_text(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = PRODUCTION is not None
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -135,6 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 ASGI_APPLICATION = 'server.routing.application'
