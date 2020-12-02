8180f673ebc0021089d28d89eec353a609f344c4
Aleks <aleks.kruza@gmail.com>
2020-12-02 16:01:02 -0500

    update prospect converter

--- api-server/src/roster/serializers.py
+++ api-server/src/roster/serializers.py
@@ -98,3 +98,9 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'password']
 
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
