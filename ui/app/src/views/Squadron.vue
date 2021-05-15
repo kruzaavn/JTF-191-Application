7de0452dde7f840e60fbc5ab286fd7afaab27eef
kruzaavn <aleks.kruza@gmail.com>
2021-05-14 20:56:19 -0400

    reserve pilots will no longer render on the webpage.

--- ui/app/src/views/Squadron.vue
+++ ui/app/src/views/Squadron.vue
@@ -12,64 +12,66 @@
               Aviators: {{ members.length }}
             </v-card-title>
           </v-img>
-          <v-card
-            class="mx-4 my-5 py-2"
-            v-for="member in members"
-            :key="member.id"
-            tile
-          >
-            <v-row>
-              <v-col cols="2">
-                <v-img
-                  max-height="150"
-                  contain
-                  :src=ranks[member.rank_code.toString()]
-                >
-                </v-img>
-              </v-col>
-              <v-col>
-                <v-card-title>
-                  {{ member.rank }} {{ member.callsign }} {{ member.position }}
-                </v-card-title>
-                <v-card-subtitle>
-                  {{ member.squadron.tri_code }}{{ member.division }}-{{
-                    member.division_position
-                  }}|{{ member.callsign }}|{{ member.tail_number }} <br />
-                  Aircraft: {{ member.tail_number }} <br />
-                  On board since {{ member.date_joined }} <br />
-                  Status: {{ member.status }}
-                </v-card-subtitle>
-              </v-col>
-            </v-row>
-            <v-card-text>
+          <div v-for="member in members" :key="member.id">
+            <v-card
+              class="mx-4 my-5 py-2"
+              v-if="member.status !== 'reserve'"
+              tile
+            >
               <v-row>
-                <v-col>
-                  <h4>
-                    Total Flight Hours
-                    {{ sumTable(member.stats.hours).toFixed(2) }}
-                  </h4>
-                  <v-data-table
-                    dense
-                    :headers="hoursHeaders"
-                    :items="toHoursTable(member.stats.hours)"
+                <v-col cols="2">
+                  <v-img
+                    max-height="150"
+                    contain
+                    :src="ranks[member.rank_code.toString()]"
                   >
-                  </v-data-table>
+                  </v-img>
                 </v-col>
                 <v-col>
-                  <h4>
-                    Total kills
-                    {{ sumTable(member.stats.kills).toFixed(0) }}
-                  </h4>
-                  <v-data-table
-                    dense
-                    :headers="killsHeaders"
-                    :items="toKillsTable(member.stats.kills)"
-                  >
-                  </v-data-table>
+                  <v-card-title>
+                    {{ member.rank }} {{ member.callsign }}
+                    {{ member.position }}
+                  </v-card-title>
+                  <v-card-subtitle>
+                    {{ member.squadron.tri_code }}{{ member.division }}-{{
+                      member.division_position
+                    }}|{{ member.callsign }}|{{ member.tail_number }} <br />
+                    Aircraft: {{ member.tail_number }} <br />
+                    On board since {{ member.date_joined }} <br />
+                    Status: {{ member.status }}
+                  </v-card-subtitle>
                 </v-col>
               </v-row>
-            </v-card-text>
-          </v-card>
+              <v-card-text>
+                <v-row>
+                  <v-col>
+                    <h4>
+                      Total Flight Hours
+                      {{ sumTable(member.stats.hours).toFixed(2) }}
+                    </h4>
+                    <v-data-table
+                      dense
+                      :headers="hoursHeaders"
+                      :items="toHoursTable(member.stats.hours)"
+                    >
+                    </v-data-table>
+                  </v-col>
+                  <v-col>
+                    <h4>
+                      Total kills
+                      {{ sumTable(member.stats.kills).toFixed(0) }}
+                    </h4>
+                    <v-data-table
+                      dense
+                      :headers="killsHeaders"
+                      :items="toKillsTable(member.stats.kills)"
+                    >
+                    </v-data-table>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </div>
         </v-card>
       </v-col>
     </v-row>
@@ -83,17 +85,28 @@ export default {
   props: ['squadronDesignation'],
   data: () => ({
     ranks: {
-      "-4": 'https://www.army.mil/e2/images/rv7/ranks/badges/warrant_officer/sm/cw1.svg',
-      "-3": 'https://www.army.mil/e2/images/rv7/ranks/badges/warrant_officer/sm/cw2.svg',
-      "-2": 'https://www.army.mil/e2/images/rv7/ranks/badges/warrant_officer/sm/cw3.svg',
-      "-1": 'https://www.army.mil/e2/images/rv7/ranks/badges/warrant_officer/sm/cw4.svg',
-      "0": 'https://www.army.mil/e2/images/rv7/ranks/badges/warrant_officer/sm/cw5.svg',
-      "1": 'https://www.army.mil/e2/images/rv7/ranks/badges/officer/sm/second_lieutenant.svg',
-      "2": 'https://www.army.mil/e2/images/rv7/ranks/badges/officer/sm/first_lieutenant.svg',
-      "3": 'https://www.army.mil/e2/images/rv7/ranks/badges/officer/sm/captain.svg',
-      "4": 'https://www.army.mil/e2/images/rv7/ranks/badges/officer/sm/major.svg',
-      "5": 'https://www.army.mil/e2/images/rv7/ranks/badges/officer/sm/lieutenant_colonel.svg',
-      "6": 'https://www.army.mil/e2/images/rv7/ranks/badges/officer/sm/colonel.svg',
+      '-4':
+        'https://www.army.mil/e2/images/rv7/ranks/badges/warrant_officer/sm/cw1.svg',
+      '-3':
+        'https://www.army.mil/e2/images/rv7/ranks/badges/warrant_officer/sm/cw2.svg',
+      '-2':
+        'https://www.army.mil/e2/images/rv7/ranks/badges/warrant_officer/sm/cw3.svg',
+      '-1':
+        'https://www.army.mil/e2/images/rv7/ranks/badges/warrant_officer/sm/cw4.svg',
+      '0':
+        'https://www.army.mil/e2/images/rv7/ranks/badges/warrant_officer/sm/cw5.svg',
+      '1':
+        'https://www.army.mil/e2/images/rv7/ranks/badges/officer/sm/second_lieutenant.svg',
+      '2':
+        'https://www.army.mil/e2/images/rv7/ranks/badges/officer/sm/first_lieutenant.svg',
+      '3':
+        'https://www.army.mil/e2/images/rv7/ranks/badges/officer/sm/captain.svg',
+      '4':
+        'https://www.army.mil/e2/images/rv7/ranks/badges/officer/sm/major.svg',
+      '5':
+        'https://www.army.mil/e2/images/rv7/ranks/badges/officer/sm/lieutenant_colonel.svg',
+      '6':
+        'https://www.army.mil/e2/images/rv7/ranks/badges/officer/sm/colonel.svg',
     },
     hoursHeaders: [
       {
