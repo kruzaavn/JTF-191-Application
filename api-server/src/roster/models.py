bd17e95d1c9a0fb1e63ef5d572f7a5860cb34693
toasterj <tonyhenderson11@hotmail.com>
2021-05-24 16:46:32 -0700

    -added squadronDescription to models.py

--- api-server/src/roster/models.py
+++ api-server/src/roster/models.py
@@ -85,6 +85,7 @@ class Squadron(models.Model):
     img = models.ImageField(upload_to='squadrons')
     callsign = models.CharField(max_length=1024, default='None')
     tri_code = models.CharField(max_length=3, default='NCS')
+    squadronDescription = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f'{self.name} - {self.designation}'
