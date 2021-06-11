6e33a8b42302c960a867e742319d044670f6ecbc
toasterj <tonyhenderson11@hotmail.com>
2021-06-10 23:35:58 -0700

    - this push will be some stuff. The last commit has a broken operation class because of one associated with the HQ class

--- ui/app/src/views/Tracker.vue
+++ ui/app/src/views/Tracker.vue
@@ -4,6 +4,13 @@
       <v-col cols="2">
       Big Hairy Balls!
       </v-col>
+      <v-data-table
+        dense
+        :headers="munitionsHeader"
+        :items="toMunitionsTable(stores)"
+        item-key="name"
+        class="elevation-1"
+      ></v-data-table>
     </v-row>
 
   </v-container>
@@ -16,32 +23,55 @@ export default {
   name: "Tracker",
   data: () => ({
         munitionsHeader: [
-        {
-          text: 'Stores',
-          align: 'start',
-          sortable: true,
-          value: 'munition',
-        },
-        {text: 'Munition', value: 'munition'},
-        {text: 'Count', value: 'count'},
-        {text: 'Category', value: 'type'},
-      ],
-      }
-    ),
-  methods: {
-    ...mapActions(['getMunitionsList','getStoresList'])
+          {
+            text: 'Stores',
+            align: 'start',
+            sortable: true,
+            value: 'type',
+          },
+          {text: 'Munition', value:'munition'},
+          {text: 'Count', value:'count'},
+          {text: 'Category', value: 'munitionType'},
+        ],
 
-  },
+      }
+  ),
+
 
   computed: {
     ...mapGetters(['munitions', 'stores']),
 
+    // eslint-disable-next-line vue/return-in-computed-property
+    mounted() {
+      this.getMunitionsList();
+      this.getStoresList();
+    },
   },
-  mounted() {
-    this.getMunitionsList();
-    this.getStoresList()
-  }
+
+  methods: {
+    ...mapActions(['getMunitionsList', 'getStoresList']),
+
+    toMunitionsTable(stores) {
+      let data = []
+
+      for (var i = 0; i < stores.length; i++) {
+        if (store.munition =  ){
+
+      }
+    }
+      /* Example Output
+      let data = {
+      "count":54,
+      "munition":4,
+      "operation":1,
+      "squadron":1}
+       */
+      return data;
+    },
+
+  },
 }
+
 </script>
 
 <style scoped>
