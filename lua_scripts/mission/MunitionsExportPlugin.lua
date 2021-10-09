68d73b3829264e0b5229295a084ebe4039dce6a8
aleks <aleks.kruza@gmail.com>
2021-10-09 11:41:05 -0400

    html emails ahoy

--- lua_scripts/mission/MunitionsExportPlugin.lua
+++ lua_scripts/mission/MunitionsExportPlugin.lua
@@ -24,7 +24,7 @@ local c = nil
 local socket = require('socket')
 local JSON = loadfile("Scripts\\JSON.lua")()
 local host = 'relay.jtf191.com'  -- change to application dns name or ip
-local port = 7225  -- change to app port
+local port = 7226  -- change to app port
 
 
 function connect_socket()
