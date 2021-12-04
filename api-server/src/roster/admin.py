a1e00fd9e31e70270f8e80cc98dbc4275a647a44
Andrea Fuggetta <afuggetta@gmail.com>
2021-12-03 19:12:18 -0500

    chore(awards): removing unnecessary import in admin.py

--- api-server/src/roster/admin.py
+++ api-server/src/roster/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.conf import settings
-from django.contrib.admin.decorators import display
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.core.mail import send_mail
