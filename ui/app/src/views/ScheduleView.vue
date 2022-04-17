a1dacb1b314481a848344d66537cb6af60f386c9
aleks <aleks.kruza@gmail.com>
2022-04-17 14:07:54 -0400

    calendar update

--- ui/app/src/views/ScheduleView.vue
+++ ui/app/src/views/ScheduleView.vue
@@ -3,79 +3,73 @@
     <v-row>
       <v-col>
         <h1>JTF Schedule</h1>
-        <FullCalendar class="calendar" :options="calendarOptions"/>
+        <FullCalendar class="calendar" :options="calendarOptions"
+          >
+        </FullCalendar>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
-import '@fullcalendar/core/vdom'
-import FullCalendar from '@fullcalendar/vue3'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionPlugin from '@fullcalendar/interaction'
+import "@fullcalendar/core/vdom";
+import FullCalendar from "@fullcalendar/vue3";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 function formatEvent(eventData) {
-  return {title: eventData.name, ...eventData}
+  return { title: eventData.name, ...eventData };
 }
 
 const eventDefaults = {
-  url: '/api/roster/event/list/',
+  url: "/api/roster/event/list/",
   eventDataTransform: formatEvent,
-  editable: true
-}
+  editable: true,
+};
 
 export default {
   name: "ScheduleView",
   components: { FullCalendar },
   data: () => ({
-    value: "",
-    selectedEvent: {},
-    selectedElement: null,
-    selectedOpen: false,
     calendarOptions: {
-      plugins: [dayGridPlugin,timeGridPlugin, interactionPlugin],
-      initialView: 'dayGridMonth',
+      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+      initialView: "dayGridMonth",
       headerToolbar: {
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek'
-        },
+        left: "prev,next today",
+        center: "title",
+        right: "dayGridMonth,timeGridWeek",
+      },
+      selectable: true,
+      selectMirror: true,
+      nowIndicator: true,
       eventSources: [
         {
-          extraParams: {type: 'operation'},
-          color: 'blue',
-          ...eventDefaults
+          extraParams: { type: "operation" },
+          color: "blue",
+          ...eventDefaults,
         },
         {
-          extraParams: {type: 'training'},
-          color: 'green',
-          ...eventDefaults
+          extraParams: { type: "training" },
+          color: "green",
+          ...eventDefaults,
         },
         {
-          extraParams: {type: 'admin'},
-          color: 'purple',
-          ...eventDefaults
-        }
+          extraParams: { type: "admin" },
+          color: "purple",
+          ...eventDefaults,
+        },
       ],
-    }
+    },
   }),
 
-  computed: {
-
-  },
-  methods: {
-
-  }
+  computed: {},
+  methods: {},
 };
 </script>
 
 <style scoped>
-
 .calendar {
-  height: 80vh;
+ height: 80vh;
 }
-
 </style>
